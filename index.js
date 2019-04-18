00fd51dbf12749810a38b25c3d682de81babbe3f
marcantoupin <marcantoupin@gmail.com>
2019-04-17 21:22:47 -0400

    Simple targets

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const globals = require('./lib/ntf-globals.js')
 const ntf = require('./lib/ntf-classes.js')
 
 let axes = ntf.Pool(40,ntf.ProjectileThrownAxe)
+let targets = ntf.Pool(40,ntf.Target)
 let ground = ntf.Ground()
 let hero = ntf.Hero()
 let paused = false
 
 //==========================================================
+
+window.setup = function() {
+  createCanvas(800, 400);
+  for (let i=0; i<10; i++) {
+    let target = targets.getNonExtant()
+    target.init(400+i*300,200*Math.random())
+  }
+}
 
 window.keyPressed = function() {
   hero.keyPressed(keyCode)
@@ -25,14 +34,26 @@ window.draw = function() {
       axe.init(hero.x+axeCenter.x,hero.y+axeCenter.y,hero.axe.throwAngle,hero.axe.swingSpeed)
     }
     ground.collide(hero)
+    targets.allExtantCall('update')
     axes.allExtantCall('update')
+    axes.testExtantAgainst(axesOnTargets,targets)
   }
   ground.draw()
+  targets.allExtantCall('draw')
   axes.allExtantCall('draw')
   hero.draw()
 }
 
-window.setup = function() {
-  console.log('setup!')
-  createCanvas(800, 400);
+let axesOnTargets = function(o1,o2){
+  if (overlapsRadius(o1,o2)){
+    o1.kill()
+    o2.kill()
+  }
+}
+
+let overlapsRadius = function(o1,o2){
+  let dx = o2.x-o1.x
+  let dy = o2.y-o1.y
+  let d = Math.sqrt(dx*dx+dy*dy)
+  return d<=o1.radius+o2.radius
 }
